f6997b8975f26dfe88c63f04b4ea4f8e27511756
Guillermo <67073007+0Memo@users.noreply.github.com>
2024-08-24 18:32:30 +0200

    Update HomeBtn.jsx

--- src/components/HomeBtn.jsx
+++ src/components/HomeBtn.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 import Link from 'next/link';
 import { Home } from 'lucide-react';
 import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
+import en from '../locales/en';
+import fr from '../locales/fr';
 
 const NavLink = motion(Link);
 
 const HomeBtn = () => {
+    const router = useRouter();
+    const { locale } = router;
+    const t = locale === 'en' ? en : fr;
+    
+    const changeLanguage = (e) => {
+        const locale = e.target.value;
+        router.push(router.pathname, router.asPath, { locale });
+    };
     return (
         <NavLink
             initial={{ scale: 0 }}
@@ -24,8 +35,16 @@ const HomeBtn = () => {
                 <Home className="w-full h-auto" strokeWidth={1.5} />
                 <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
                 <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
-                    Accueil
+                    {t.welcomePage}
                 </span>
+                <select
+                    onChange={changeLanguage}
+                    defaultValue={locale}
+                    className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
+                  >
+                    <option className="text-black" value="en">FR</option>
+                    <option className="text-black" value="fr">EN</option>
+                  </select>
             </span>                
         </NavLink>
     )
