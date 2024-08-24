b015d4c31b5a99261a613ee719d982a520f869b8
Guillermo <67073007+0Memo@users.noreply.github.com>
2024-08-24 18:33:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
+    i18n: {
+        locales: ['en', 'fr'],
+        defaultLocale: 'en',
+    },
     reactStrictMode: true,
 };
 
