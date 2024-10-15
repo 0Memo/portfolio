7845d92ed27122e3fe2ee3cdafea837278cd32bc
Guillermo <67073007+0Memo@users.noreply.github.com>
2024-10-15 10:52:26 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        <link rel="icon" href="/favicon.ico"  type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico?v=2  type="image/x-icon" />
       </head>
       <body className={clsx(inter.variable, 'bg-background text-foreground font-inter')}>
         {children}
