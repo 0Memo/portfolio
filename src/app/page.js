7b44ca8353d63ce289b8348bc3273c083882ac38
Guillermo <67073007+0Memo@users.noreply.github.com>
2024-12-19 11:22:08 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import RenderModel from "@/components/RenderModel";
 // import Wizard from "@/components/models/Wizard";
 import Navigation from "@/components/navigation";
 import dynamic from "next/dynamic";
+import './page.css';
+import './page_custom.js';
 
 const Wizard = dynamic(() => import('@/components/models/Wizard2'), {ssr: false});
 
@@ -17,6 +19,13 @@ export default function Home() {
           <Wizard />
         </RenderModel>
       </div>
+      <div class="clouds-bg"></div>
+      <canvas id="snow-canvas-1"></canvas>
+      <canvas id="snow-canvas-2"></canvas>
+      <canvas id="snow-canvas-3"></canvas>
+      <canvas id="snow-canvas-4"></canvas>
+      <canvas id="snow-canvas-5"></canvas>
+      <canvas id="snow-canvas-6"></canvas>
       <p className="fixed bottom-0 text-custom mb-2 font-semibold text-md">&copy; Guillermo 2024 | Tous droits réservés</p>
     </main>
   );
