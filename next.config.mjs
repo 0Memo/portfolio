ea84672796c9561e4ec6ae2c894b809b79435446
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-01-01 09:20:43 +0100

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const withNextIntl = createNextIntlPlugin(path.resolve('./src/i18n/request.ts'));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.json$/,
+            include: path.resolve(__dirname, 'src/messages'),
+            type: 'json',
+        });
+        return config;
+    },
 };
 
 export default withNextIntl(nextConfig);
